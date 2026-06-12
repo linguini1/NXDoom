d1f9a8c4b666f3692ca958318ab254427a26bc48
Matteo Golin <matteo.golin@gmail.com>
2026-06-12 16:59:54 -0400

    chore: style

--- src/memio.h
+++ src/memio.h
@@ -1,20 +1,29 @@
-//
-// Copyright(C) 1993-1996 Id Software, Inc.
-// Copyright(C) 2005-2014 Simon Howard
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
+/****************************************************************************
+ * apps/games/NXDoom/src/memio.h
+ *
+ * SPDX-License-Identifer: GPLv2
+ *
+ * Copyright(C) 1993-1996 Id Software, Inc.
+ * Copyright(C) 2005-2014 Simon Howard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ ****************************************************************************/
 
 #ifndef MEMIO_H
 #define MEMIO_H
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
 
 typedef struct _MEMFILE MEMFILE;
 
@@ -24,6 +33,10 @@ typedef enum
   MEM_SEEK_CUR,
   MEM_SEEK_END,
 } mem_rel_t;
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
 
 MEMFILE *mem_fopen_read(void *buf, size_t buflen);
 size_t mem_fread(void *buf, size_t size, size_t nmemb, MEMFILE *stream);
@@ -35,4 +48,4 @@ void mem_fclose(MEMFILE *stream);
 long mem_ftell(MEMFILE *stream);
 int mem_fseek(MEMFILE *stream, signed long offset, mem_rel_t whence);
 
-#endif /* #ifndef MEMIO_H */
+#endif /* MEMIO_H */
