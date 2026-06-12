4556c6fa0719cef8b453f503303542378710f61e
Matteo Golin <matteo.golin@gmail.com>
2026-06-12 13:05:44 -0400

    chore: style

--- src/p_rejectpad.c
+++ src/p_rejectpad.c
@@ -1,20 +1,29 @@
-//
-// Copyright(C) 1993-1996 Id Software, Inc.
-// Copyright(C) 2005-2014 Simon Howard
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// DESCRIPTION:
-// Padding of Reject Lump
-//
+/****************************************************************************
+ * apps/games/NXDoom/src/p_rejectpad.c
+ *
+ * SPDX-License-Identifer: GPLv2
+ *
+ * Copyright(C) 1993-1996 Id Software, Inc.
+ * Copyright(C) 2005-2014 Simon Howard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * DESCRIPTION:
+ * Padding of Reject Lump
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
 #include <stdio.h>
 #include <string.h>
@@ -22,8 +31,13 @@
 #include "m_argv.h"
 #include "p_rejectpad.h"
 
-// Pad the REJECT lump with extra data when the lump is too small,
-// to simulate a REJECT buffer overflow in Vanilla Doom.
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/* Pad the REJECT lump with extra data when the lump is too small,
+ * to simulate a REJECT buffer overflow in Vanilla Doom.
+ */
 
 void pad_reject_array(byte *array, unsigned int len, int totallines)
 {
@@ -32,18 +46,19 @@ void pad_reject_array(byte *array, unsigned int len, int totallines)
   byte *dest;
   unsigned int padvalue;
 
-  // Values to pad the REJECT array with:
+  /* Values to pad the REJECT array with: */
 
-  unsigned int rejectpad[4] = {
-      0,       // Size
-      0,       // Part of z_zone block header
-      50,      // PU_LEVEL
-      0x1d4a11 // DOOM_CONST_ZONEID
+  unsigned int rejectpad[4] =
+  {
+    0,       /* Size */
+    0,       /* Part of z_zone block header */
+    50,      /* PU_LEVEL */
+    0x1d4a11 /* DOOM_CONST_ZONEID */
   };
 
   rejectpad[0] = ((totallines * 4 + 3) & ~3) + 24;
 
-  // Copy values from rejectpad into the destination array.
+  /* Copy values from rejectpad into the destination array. */
 
   dest = array;
 
@@ -54,8 +69,9 @@ void pad_reject_array(byte *array, unsigned int len, int totallines)
       ++dest;
     }
 
-  // We only have a limited pad size.  Print a warning if the
-  // REJECT lump is too small.
+  /* We only have a limited pad size.  Print a warning if the
+   * REJECT lump is too small.
+   */
 
   if (len > sizeof(rejectpad))
     {
@@ -63,7 +79,8 @@ void pad_reject_array(byte *array, unsigned int len, int totallines)
               "pad_reject_array: REJECT lump too short to pad! (%u > %i)\n",
               len, (int)sizeof(rejectpad));
 
-      // Pad remaining space with 0 (or 0xff, if specified on command line).
+      /* Pad remaining space with 0 (or 0xff, if specified on command line).
+       */
 
       if (m_check_parm("-reject_pad_with_ff"))
         {
