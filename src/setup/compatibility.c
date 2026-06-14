fb3a83b6d2f86bfb6a1d6e2d5b04703c564f1ebc
Matteo Golin <matteo.golin@gmail.com>
2026-06-14 14:58:27 -0400

    chore: style

--- src/setup/compatibility.c
+++ src/setup/compatibility.c
@@ -1,18 +1,27 @@
-//
-// Copyright(C) 2005-2014 Simon Howard
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
+/****************************************************************************
+ * apps/games/NXDoom/src/setup/compatibility.c
+ *
+ * SPDX-License-Identifer: GPLv2
+ *
+ * Copyright(C) 2005-2014 Simon Howard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Sound control menu
+ *
+ ****************************************************************************/
 
-// Sound control menu
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
 #include <stdlib.h>
 
@@ -22,10 +31,22 @@
 
 #include "compatibility.h"
 
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
 #define WINDOW_HELP_URL "https://www.chocolate-doom.org/setup-compat"
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
 
 int vanilla_savegame_limit = 1;
 int vanilla_demo_limit = 1;
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
 
 void compatiblity_settings(TXT_UNCAST_ARG(widget), void *user_data)
 {
