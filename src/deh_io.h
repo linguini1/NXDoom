65d4a90a9530ddb835908c1f5a034b0b3177e34f
Matteo Golin <matteo.golin@gmail.com>
2026-06-14 12:56:59 -0400

    chore: style

--- src/deh_io.h
+++ src/deh_io.h
@@ -1,24 +1,36 @@
-//
-// Copyright(C) 2005-2014 Simon Howard
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-//
-// Dehacked I/O code (does all reads from dehacked files)
-//
+/****************************************************************************
+ * apps/games/NXDoom/src/deh_io.h
+ *
+ * SPDX-License-Identifer: GPLv2
+ *
+ * Copyright(C) 2005-2014 Simon Howard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Dehacked I/O code (does all reads from dehacked files)
+ *
+ ****************************************************************************/
 
 #ifndef DEH_IO_H
 #define DEH_IO_H
 
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
 #include "deh_defs.h"
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
 
 deh_context_t *deh_open_file(const char *filename);
 deh_context_t *deh_open_lump(int lumpnum);
@@ -31,4 +43,4 @@ void deh_warning(deh_context_t *context, const char *msg, ...)
     PRINTF_ATTR(2, 3);
 boolean deh_had_error(deh_context_t *context);
 
-#endif /* #ifndef DEH_IO_H */
+#endif /* DEH_IO_H */
